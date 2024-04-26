52150a277480ca2eaf002d7d77d9421a6fee263c
Shixun Wu <shixun404@gmail.com>
2024-04-26 12:47:48 -0700

    Update matmul.c

--- matmul.c
+++ matmul.c
@@ -10,55 +10,78 @@ int _initialize(double * matrix);
 int main()
 {
 	int i, j, k;
-	double *A, *B, *Cs, *Cp;
+	double *A, *B, *C1, *C2, *C3;
 	struct timeval start, end;
 	double timeCost;
 
 	A=(double *)malloc(sizeof(double)*N*N);
 	B=(double *)malloc(sizeof(double)*N*N);
-	Cs=(double *)malloc(sizeof(double)*N*N);
-	Cp=(double *)malloc(sizeof(double)*N*N);
+	C1=(double *)malloc(sizeof(double)*N*N);
+	C2=(double *)malloc(sizeof(double)*N*N);
+	C3=(double *)malloc(sizeof(double)*N*N);
 
 	initialize(A);
 	initialize(B);
-	_initialize(Cs);
-	_initialize(Cp);
+	_initialize(C1);
+	_initialize(C2);
+	_initialize(C3);
 
-	// sequential version
+	// Version 1
 	gettimeofday(&start, NULL);
 	for(i=0; i<N; i++)
 	    for(j=0; j<N; j++)
 		    for(k=0; k<N; k++)
-		        Cs[i*N+j]+=A[i*N+k]*B[k*N+j];
+		        C1[i*N+j]+=A[i*N+k]*B[k*N+j];
 	gettimeofday(&end, NULL);
 
 	timeCost=1000000*(end.tv_sec-start.tv_sec)+(end.tv_usec-start.tv_usec);
 	timeCost/=1000000;
-    printf("The squential version of matrix multiplication costs %lf seconds\n", timeCost);
+        printf("Version-1 costs %lf seconds\n", timeCost);
 
 	//////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////
-	// please change this into a parallel version
+	// Version 2
 	gettimeofday(&start, NULL);
-	for(i=0; i<N; i++)
-	    for(j=0; j<N; j++)
-	    	for(k=0; k<N; k++)
-		       Cp[i*N+j]+=A[i*N+k]*B[k*N+j];
+	/* 
+		To be filled by student.
+	*/ 
 	gettimeofday(&end, NULL);
 
 	timeCost=1000000*(end.tv_sec-start.tv_sec)+(end.tv_usec-start.tv_usec);
 	timeCost/=1000000;
-	printf("The parallel version of matrix multiplication costs %lf seconds\n", timeCost);
+	printf("Version-2 costs %lf seconds\n", timeCost);
 	//////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////
+	// Version 3
+	gettimeofday(&start, NULL);
+	/* 
+		To be filled by student.
+	*/ 
+	gettimeofday(&end, NULL);
 
+	timeCost=1000000*(end.tv_sec-start.tv_sec)+(end.tv_usec-start.tv_usec);
+	timeCost/=1000000;
+	printf("Version-2 costs %lf seconds\n", timeCost);
+	//////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
 	// verification
 	for(i=0; i<N; i++)
 	  for(j=0; j<N; j++) {
-            if (Cs[i*N+j] - Cp[i*N+j] < 0.000001)
+            if (C1[i*N+j] - C2[i*N+j] < 0.000001)
                 continue;
             else {
-                printf("ERRORS DETECTED!!!!!!!!!!!!!\n");
+                printf("Version 2 ERRORS DETECTED!!!!!!!!!!!!!\n");
+                return -1;
+            }
+        } 
+
+	
+	for(i=0; i<N; i++)
+	  for(j=0; j<N; j++) {
+            if (C1[i*N+j] - C3[i*N+j] < 0.000001)
+                continue;
+            else {
+                printf("Version 3 ERRORS DETECTED!!!!!!!!!!!!!\n");
                 return -1;
             }
         } 
@@ -66,8 +89,9 @@ int main()
 	// release resources and return
 	free(A);
 	free(B);
-	free(Cs);
-	free(Cp);
+	free(C1);
+	free(C2);
+	free(C3);
 	
 	return 0;
 }
